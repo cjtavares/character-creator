232f77bbe17ad55e87a08b989963bb67baab09bf
davjh <dillonsdad91@gmail.com>
2022-10-31 01:30:34 -0400

    added css styling to sign up page

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,9 +1,49 @@
 const router = require('express').Router();
 const { Characters, Users } = require('../models');
-const withAuth = require("../utils/auth");
 
-router.get('/signup', withAuth, (req, res) => {
+
+router.get('/', (req, res) => {
     res.render('signup')
     });
 
+    async function addNewUser(event) {
+        event.preventDefault();
+        const newUser = document.querySelector('.newUser').value;
+        const password = document.querySelector('.password').value;
+        const rPassword = document.querySelectorAll('.rPassword').value;
+
+        // if(rPassword !== Password) {
+
+            console.log("newUser")
+
+            const response = await fetch('/api/createChar', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+        //         body: JSON.stringify({
+        //             password: password
+        //             new_user: newUser
+        //              r_password: rPassword
+        //             secret: secret,
+                }) 
+            // });
+        // }
+        }
+
+        router.post('/', function (req, res) {
+            res.sendFile(path.join(__dirname, ""))
+        })
+
+
+
+    //     console.log(addNewUser);
+
+    // }, 
+    // body: JSON.stringify({
+    //     new_user: newUser,
+    //     password: password,
+
+    
 module.exports = router;
