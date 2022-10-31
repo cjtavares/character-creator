8f440f4af94f082e090a5263f4e7ed3315cf8857
cjtavares <105406233+cjtavares@users.noreply.github.com>
2022-10-31 12:59:11 -0400

    working on css

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,19 +6,6 @@ res.render('login', {
     layout: 'login',
     logged_in: req.session.logged_in
 })
-
 });
-
-// router.post('/logout', (req, res) => {
-//     if (req.session.logged_in) {
-//       req.session.destroy(() => {
-//         res.status(204).end();
-//       });
-//     } else {
-//       res.status(404).end();
-//     }
-//   });
-
-
 
 module.exports = router;
--- controllers/signup.js
+++ controllers/signup.js
@@ -3,7 +3,10 @@ const { Characters, Users } = require('../models');
 
 
 router.get('/', (req, res) => {
-    res.render('signup')
+    res.render('signup', {
+        layout: 'login',
+        logged_in: req.session.logged_in
+    })
     });
     
 module.exports = router;
